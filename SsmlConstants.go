2134041ee05fab6196f517b6aade4f3ebf383034
Shawn Eion Smith <ses44@psu.edu>
2018-04-09 13:19:58 -0400

    Adding an ssml constants file

---
+++ SsmlConstants.go
@@ -0,0 +1,51 @@
+package alexatools
+
+//This block is the const values for SSML Processing
+const (
+	//No Break Strength
+	None string = "none"
+	//Extra Weak Break Strength
+	ExtraWeak string = "x-weak"
+	//Weak Break Strength
+	Weak string = "weak"
+	//Medium Break Strenght or Medium Pitch or Medium Rate or Medium Volume
+	Medium string = "medium"
+	//Strong Break Strength or Strong Emphasis Type
+	Strong string = "strong"
+	//Extra Strong Break Strength
+	ExtraStrong string = "x-strong"
+
+	//Moderate emphasis
+	Moderate string = "moderate"
+	//Reduced emphasis
+	Reduced string = "reduced"
+
+	//Extra Low Pitch
+	ExtraLow string = "x-low"
+	//Low Pitch
+	Low string = "low"
+	//High Pitch
+	High string = "high"
+	//Extra High Pitch
+	ExtraHigh = "x-high"
+
+	//Extra Slow Rate
+	ExtraSlow string = ("x-slow")
+	//Slow Rate
+	Slow string = "slow"
+	//Fast Rate
+	Fast string = "fast"
+	//Extra Fast Rate
+	ExtraFast string = "x-fast"
+
+	//Silent Volume
+	Silent string = "silent"
+	//Extra Soft Volume
+	ExtraSoft string = "x-soft"
+	//Soft Volume
+	Soft string = "soft"
+	//Loud Volume
+	Loud string = "loud"
+	//Extra Loud Volume
+	ExtraLoud string = "x-loud"
+)
